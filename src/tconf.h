7f94b8c87031517266384878b53a05e9e2f14407
Adam Cowdy <cowdyadam@gmail.com>
2017-11-01 00:13:17 +0000

    Initial commit

---
+++ src/tconf.h
@@ -0,0 +1,21 @@
+// Copyright 2017 Adam Cowdy
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TCONF_H
+#define TCONF_H
+
+// Temporary symbol, will be removed later
+void print_hello();
+
+#endif
